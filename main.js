a36d1bc099c27a30b59182015ef63d689e8a0966
Dawid Dzierzega <70227471+hektus@users.noreply.github.com>
2023-10-23 19:36:16 +0200

    add quizbox

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const startBtn = document.querySelector(".header__button");
 const quizGuide = document.querySelector(".pop-out");
+const exitBtn = document.querySelector(".pop-out__btn--exit");
+const header = document.querySelector(".header");
+const continueBtn = document.querySelector(".pop-out__btn--continue");
+const quizPage = document.querySelector(".quiz_page");
+const body = document.querySelector("body");
+const quizBox = document.querySelector(".quiz-box");
 
 startBtn.addEventListener("click", () => {
   quizGuide.style.display = "block";
+  header.classList.add("filter");
 });
+
+exitBtn.addEventListener("click", () => {
+  quizGuide.style.display = "none";
+  header.classList.remove("filter");
+});
+
+continueBtn.addEventListener("click", () => {
+  quizPage.style.left = "0%";
+  header.classList.remove("filter");
+  quizGuide.style.display = "none";
+  showQuizbox();
+});
+
+function showQuizbox() {
+  setTimeout(function () {
+    quizBox.style.display = "block";
+  }, 1000);
+}
